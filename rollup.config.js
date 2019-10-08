74e663feb77ea736fa12d0146a04b06fc516fec5
Jonathan Hudak <hudak.jonathan@gmail.com>
2019-10-07 20:57:16 -0700

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import babel from "rollup-plugin-babel";
+import babel from 'rollup-plugin-babel';
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: {
-    file: "index.js",
-    format: "esm"
+    file: 'index.js',
+    format: 'cjs'
   },
   plugins: [babel()]
 };
